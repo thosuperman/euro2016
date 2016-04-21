7337fd49586e4ab875971cf65d217e92c6354d10
aakash-cr7 <aakash.tyg93@gmail.com>
2016-04-21 17:41:43 +0530

    Navbar added

--- euro2016/euro2016/settings/common.py
+++ euro2016/euro2016/settings/common.py
@@ -63,15 +63,15 @@ MEDIA_URL = '/media/'
 
 ########## STATIC FILE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
+#STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'assets')),
-)
+STATICFILES_DIRS = [
+    normpath(join(SITE_ROOT, 'static')),
+]
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
